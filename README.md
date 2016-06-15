9965b4ad6edf3bf32cb4235c6952b4c079fcc54a
Matt <leiboldm@umich.edu>
2016-06-14 22:58:13 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # CollegeRanking
-An algorithm to rank US 4 year colleges for display on mattleibold.com/colleges
+A python algorithm to rank US 4-year colleges for display on mattleibold.com/colleges
+ 
+College data from http://nces.ed.gov/ipeds/datacenter/
+
+Current rankings:
+
+1  Stanford University                                4761.40
+2  Harvard University                                 4575.89
+3  Yale University                                    4467.32
+4  University of Virginia-Main Campus                 4403.26
+5  University of Michigan-Ann Arbor                   4251.32
+6  Princeton University                               4244.29
+7  Columbia University in the City of New York        4190.82
+8  Massachusetts Institute of Technology              4151.11
+9  California Institute of Technology                 4046.82
+10 United States Military Academy                     4001.02
+11 University of Chicago                              3978.10
+12 Brown University                                   3909.06
+13 University of Pennsylvania                         3746.67
+14 Texas A & M University-College Station             3732.38
+15 University of California-Berkeley                  3699.19
+16 Virginia Military Institute                        3695.43
+17 College of the Ozarks                              3671.65
+18 United States Air Force Academy                    3641.55
+19 Duke University                                    3636.76
+20 Dartmouth College                                  3631.07
+21 Claremont McKenna College                          3627.96
+22 University of North Carolina at Chapel Hill        3619.48
+23 Pomona College                                     3605.42
+24 Georgia Institute of Technology-Main Campus        3596.30
+25 College of William and Mary                        3593.30
---
+++ rank.py
@@ -0,0 +1,130 @@
+import pandas
+
+def main():
+    coll_df = pandas.read_csv("collegeData.csv")
+    fillNulls(coll_df, "ProfessorSalary")
+    fillNulls(coll_df, "PercentUndergradForeign")
+    fillNulls(coll_df, "PercentUndergradOutOfState")
+    fillNulls(coll_df, "PercentUndergradInState")
+    fillNulls(coll_df, "PercentUndergrad18-24")
+    fillNulls(coll_df, "GraduationRate")
+    fillNulls(coll_df, "RetentionRate")
+    fillNulls(coll_df, "Tuition", defaultValue=0) # military academies are free
+    fillNulls(coll_df, "Endowment")
+    coll_df['score'] = coll_df.apply(lambda row: createScore(row), axis=1)
+
+    top_colleges = coll_df.sort('score', ascending=False)
+    rank = 1
+    for i, row in top_colleges[:25][['name', 'score']].iterrows():
+        print "{} {} {:.2f}".format(str(rank).ljust(2), row['name'].ljust(50), row['score'])
+        rank += 1
+
+def fillNulls(df, key, defaultValue=None):
+    if defaultValue == None: 
+        defaultValue = df[key].mean()
+    df[key].fillna(defaultValue, inplace=True)
+
+def createScore(row):
+    endowmentPerStudent = row['Endowment'] / row['Enrollment']
+
+    # calculate admission rate
+    if row['ApplicantsTotal'] > 0:
+        admissionRate = row['AdmissionsTotal'] / row['ApplicantsTotal']
+    else:
+        admissionRate = 1
+    if admissionRate > 1:
+        raise Exception("admission rate invalid for {} {}".format(row['name'], admissionRate))
+
+    # calculate matriculation rate
+    if row['AdmissionsTotal'] > 0:
+        matriculationRate = row['EnrolledTotal'] / row['AdmissionsTotal']
+    else:
+        matriculationRate = 0
+    if matriculationRate > 1:
+        raise Exception("matriculation rate invalid for {} {}".format(row['name'], matriculationRate))
+
+    # calculate 75% average SAT/SAT equivalent
+    act75 = 0
+    sat75 = row['SATReading75'] + row['SATMath75'] + row['SATWriting75']
+    dividend = 2
+    if isNaN(sat75):
+        sat75 = 0
+        dividend = 1
+    if not isNaN(row['ACT75']):
+        act75 = ACTtoSAT(row['ACT75'])
+    else:
+        dividend = 1
+    avgSAT75 = (sat75 + act75) / dividend
+
+    # calculate 25% average SAT/SAT equivalent
+    act25 = 0
+    sat25 = row['SATReading25'] + row['SATMath25'] + row['SATWriting25']
+    dividend = 2
+    if isNaN(sat25):
+        sat25 = 0
+        dividend = 1
+    if not isNaN(row['ACT25']):
+        act25 = ACTtoSAT(row['ACT25'])
+    else:
+        dividend = 1
+    avgSAT25 = (sat25 + act25) / dividend
+
+    SAT = (avgSAT25 + avgSAT75) / 2
+   
+    score = (row['Endowment'] / 200
+            + (100 / admissionRate)
+            + (matriculationRate * 100)
+            + (SAT * 1)
+            + ((50000 - row['Tuition']) / 100)
+            + (100 / row['StudentToFacultyRatio'])
+            + ((row['PercentUndergradOutOfState'] + row['PercentUndergradForeign']))
+            + (row['PercentUndergrad18-24']) # prioritize youthful schools
+            + (50 - abs(50 - row['PercentWomen'])) # prioritize equal gender ratio
+            + (row['GraduationRate'])
+            + (row['RetentionRate'])
+            + (row['ProfessorSalary'] / 10000)
+            )
+
+    return score
+
+def isNaN(val):
+    return not (val == val)
+
+# convert ACT to SAT (2400), from blog.prepscholar.com/act-to-sat-conversion
+def ACTtoSAT(actScore):
+    if actScore > 36 or actScore < 0:
+        raise Exception("ACT score invalid: {}".format(actScore))
+    if actScore < 11:
+        return 600
+    mapping = {
+        11: 780,
+        12: 870,
+        13: 950,
+        14: 1020,
+        15: 1100,
+        16: 1170,
+        17: 1230,
+        18: 1290,
+        19: 1350,
+        20: 1410,
+        21: 1470,
+        22: 1530,
+        23: 1590,
+        24: 1650,
+        25: 1710,
+        26: 1770,
+        27: 1820,
+        28: 1880,
+        29: 1940,
+        30: 2000,
+        31: 2060,
+        32: 2120,
+        33: 2180,
+        34: 2250,
+        35: 2330,
+        36: 2390 
+    }
+    return mapping[actScore]
+
+if __name__ == "__main__":
+    main()
