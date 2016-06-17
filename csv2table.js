cac0167bb7b4b2291df3e255bb6f87f0b1a91f9c
Matt <leiboldm@umich.edu>
2016-06-16 22:01:53 -0700

    webpage stuff

--- csv2table.js
+++ csv2table.js
@@ -3,6 +3,7 @@
 //         table is an jquery table element the csv will be loaded into
 // REQUIRES: jquery
 function csv2table(csvurl, table, callback) {
+    var loader = $("<img src='circle-loading-animation.gif'>").appendTo(table);
     $.ajax({
         url: csvurl,
         beforeSend: function(xhr) {
@@ -11,27 +12,30 @@ function csv2table(csvurl, table, callback) {
     }).done(function(data, status) {
         console.log(status); 
         var lines = data.split("\n");
-        var tableHtml = "<thead><tr>";
+        var headerHtml = "<thead><tr>";
         var headers = lines[0].split(",");
         for (var i = 0; i < headers.length; i++) {
-            tableHtml += ("<th>" + headers[i].split('"').join("") + "</th>");
+            headerHtml += ("<th>" + headers[i].split('"').join("") + "</th>");
         }
-        tableHtml += "</tr></thead>\n";
-        tableHtml += "<tbody>";
+        headerHtml += "</tr></thead>\n";
+        table.append(headerHtml);
+        tableBody = table.append("<tbody></tbody>");
         for (var i = 1; i < lines.length; i++) {
             if (lines[i].length == 0) continue;
             cols = lines[i].split(",");
-            tableHtml += "<tr>";
+            tableHtml = "<tr>";
             for (var j = 0; j < cols.length; j++) {
                 tableHtml += ("<td>" + cols[j].split('"').join("") + "</td>");
             }
             tableHtml += "</tr>"
+            tableBody.append(tableHtml);
         }
-        tableHtml += "</tbody>";
-        table.html(tableHtml);
+        tableBody.append(tableHtml);
     }).fail(function (xhr, status, error) {
         console.log(status);
+        table.html("Failed to load data");
     }).always(function() {
+        loader.remove();
         callback();
     });
 }
